aa25137baa5680a2adeffe89a29fed911eff57a2
Эрик <erik.tsarev@gmail.com>
2024-05-24 23:20:59 +0300

    del

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ class TestSolverFunction(unittest.TestCase):
     @patch('builtins.print')
     def test_solver_function(self, mock_print):
         solver(5, 10)
-        mock_print.assert_called_with('5 10')
+        mock_print.assert_called_with('510')
 
 if __name__ == '__main__':
     unittest.main()
